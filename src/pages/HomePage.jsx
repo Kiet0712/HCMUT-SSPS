372f697eea5693676c4bd4bd7532f0e6a67c00c4
Tram <tramdang7907129@gmail.com>
2024-11-17 22:46:41 +0700

    Add Homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import StickyNavBar from "../components/StickyNavBar";
+import { Welcome } from "../components/Welcome";
+
+export default function HomePage() {
+    return (
+        <div>
+            <StickyNavBar />
+            <Welcome />
+        </div>
+    )
+}
